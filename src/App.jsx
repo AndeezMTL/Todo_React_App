4ddd17f9678fb681e463fa3cc3e63a1e7deb2399
Andy lamothe <142926439+AndeezMTL@users.noreply.github.com>
2024-04-03 09:17:22 -0400

    Ajout de mon dossier react

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import TodoForm from './Composants/TodoForm';
+import Todolist from './Composants/Todolist';
+
+
+/** 
+ * 
+*/
+
+
+
+
+function App() {
+  return (
+    <div className='todo-app'>
+    <Todolist />
+    </div>
+  );
+}
+
+export default App;
